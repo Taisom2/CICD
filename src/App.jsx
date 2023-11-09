71d11f677b1cef485e8efdf6de1fa60c9c2d79bc
Taisom2 <julianandresgarciavasquez66@gmail.com>
2023-11-09 17:40:04 -0500

    3th mommit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>A Nicolas arboleda le gusta la Berenjena</h1>
+        <h1>Brandon Garcia Marin - Dahiana Largo - Julian Garcia</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
